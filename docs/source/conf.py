eb37930d4fe377f7b9975c2d80f08706ff487900
danmaclean <maclean.daniel@gmail.com>
2019-11-20 16:15:00 +0000

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, "/Users/macleand/PycharmProjects/cgr_view")
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
